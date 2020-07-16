1d68aa024d27cba146f0ee7cbbda329c44881d33
Lorena Udroiu <lorena.danciu@yahoo.com>
2020-07-16 16:54:31 +0300

    withdraw multiple time test for negative

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,20 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int balance = 0;
+
+    public int balance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+         if (amount < 0) {
+             throw new IllegalArgumentException("Amount must be > 0");
+         }
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
+    public void newAccountShouldHaveZeroBalance() {
+        Account account = new Account();
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+    @Test
     public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
+    }
+
+    @Test
+    public void withdrawAnAmountToDecreaseTheBalance() {
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
+
+        account.withdraw(5);
+        account.withdraw(5);
+        assertThat(account.balance()).isEqualTo(20);
+    }
+
+    @Test
+    public void depositNegativeAmount() {
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
+        try {
+            //should this be considered to be an withdraw??
+            //instead of having an extra method for withdraw amount?
+            account.deposit(-5);
+        } catch (Exception ex) {
+            //check the balance is the same
+            assertThat(account.balance()).isEqualTo(30);
+        }
+
+        account.withdraw(5);
+        assertThat(account.balance()).isEqualTo(25);
     }
 }
